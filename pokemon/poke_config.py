0f583972a1f7636257c8895683fa488e446d02f4
Samantha Tang <samanthatang@Samanthas-MacBook-Air-496.local>
2024-12-15 22:15:49 -0500

    pokemon

--- pokemon/poke_config.py
+++ pokemon/poke_config.py
@@ -1,3 +1,6 @@
-POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
-DATABASE_PATH = "data/pokemon_data.sqlite3"
+import os
 
+# Unified database path for both projects
+DB_PATH = os.path.join(os.path.dirname(__file__), "../database/Movies.db")
+
+POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
